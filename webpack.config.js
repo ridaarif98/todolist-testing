f5bd0b620913bd479377e4904012c41088408915
ridaarif98 <ridaarif16@gmail.com>
2021-08-23 11:26:42 +0200

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,10 @@ module.exports = {
   })],
   module: {
     rules: [
-        {
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-        },
+      },
     ],
   },
 };
